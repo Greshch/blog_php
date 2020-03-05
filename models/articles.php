f9cc748a560c0c6815a280b43c9ae755989e602b
Greshch <greshchook@gmail.com>
2020-03-05 07:00:52 +0200

    article.php entry point

--- models/articles.php
+++ models/articles.php
@@ -2,12 +2,16 @@
 
 function articles_all()
 {
-
+    return [
+      ['id' => 1, 'title' => "Title1" , 'date' => "2015-01-01", 'content' => "Content1"],
+      ['id' => 2, 'title' => "Title2" , 'date' => "2015-01-02", 'content' => "Content2"],
+    ];
 }
 
 function articles_get($id)
 {
-
+    return ['id' => 1, 'title' => "Это простой заголовок" , 'date' => "2015-01-01",
+        'content' => "Здесь будет статья"];
 }
 
 function articles_new($title, $date, $content)
