271a1f311513cd1ff687b6089e7c74bf0b206397
Greshch <greshchook@gmail.com>
2020-03-05 21:57:03 +0200

    articles_admi.php

---
+++ views/articles_admin.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Table</title>
+</head>
+<body>
+
+
+<table border="1">
+    <tr>
+        <th>Дата</th>
+        <th>Заголовок</th>
+        <th></th>
+        <th></th>
+    </tr>
+
+    <tr>
+        <td>2015-01-01</td>
+        <td>Заголовок 1</td>
+        <td><a href="#">Редактировать</a></td>
+        <td><a href="#">Удалить</a></td>
+    </tr>
+
+    <tr>
+        <td>2015-01-01</td>
+        <td>Заголовок 2</td>
+        <td><a href="#">Редактировать</a></td>
+        <td><a href="#">Удалить</a></td>
+    </tr>
+
+    <tr>
+        <td>2015-01-01</td>
+        <td>Заголовок 3</td>
+        <td><a href="#">Редактировать</a></td>
+        <td><a href="#">Удалить</a></td>
+    </tr>
+
+</table>
+
+</body>
+</html>
