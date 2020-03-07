c793cf405beb823843d1da3f464057eea892e579
Greshch <greshchook@gmail.com>
2020-03-07 09:09:57 +0200

    12 action="index.php?action=<?=['action']?>&id=<?= ['id']?>"

--- views/article_admin.php
+++ views/article_admin.php
@@ -12,7 +12,7 @@
 
 <h1>Мой первый Блог</h1>
 
-<form action="index.php?action=add" method="post" class="form-item">
+<form action="index.php?action=<?=$_GET['action']?>&id=<?= $_GET['id']?>" method="post" class="form-item">
     <label>Название<br>
     <input type="text" name="title" autofocus required value="<?= $article['title'] ?>"></label><br>
     <label>Дата<br>
